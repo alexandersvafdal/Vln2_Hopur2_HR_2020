da371d99f29016c5d32dae1e3a541ecc3f66ab94
alexandersvafdal <alexander18@ru.is>
2020-04-27 15:08:43 +0000

    hope that this goes well , barbones captain django project

---
+++ project/Captain_Console/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\alexa\OneDrive\Skjöl\háskóli 2020\Þriggja Vikna Námskeið\Verkefnið\project\Captain_Console\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
