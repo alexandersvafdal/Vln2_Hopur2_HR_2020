ee296e24612838a9f5f56484519d2e1ad4037835
alexandersvafdal <alexander18@ru.is>
2020-05-06 10:51:36 +0000

    removed venv from git repo

--- project/Captain_Console/Captain/models.py
+++ project/Captain_Console/Captain/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
+
 class Products(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=999, blank=True)
@@ -19,6 +23,7 @@ class Products(models.Model):
 class ProductImage(models.Model):
     image = models.CharField(max_length=999)
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
+
 
 
 
