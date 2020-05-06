87eba3b3063b858a937159555bcd1f30fa2d4c64
alexandersvafdal <alexander18@ru.is>
2020-05-06 10:55:55 +0000

    removed Captain_console filder up a few dirs

---
+++ Captain_Console/Captain/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=999, blank=True)
+    photo = models.CharField(max_length=999)
+    price = models.FloatField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    manufacturer = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    image = models.CharField(max_length=999)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+
+
+
+
