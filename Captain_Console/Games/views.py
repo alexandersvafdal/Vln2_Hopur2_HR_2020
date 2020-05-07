3f81a518cf83c77aea39da7217bf9a2012de4b38
alexandersvafdal <alexander18@ru.is>
2020-05-07 12:44:43 +0000

    Sindri added pictures to db. , changed detailproduct and category template.

--- Captain_Console/Games/views.py
+++ Captain_Console/Games/views.py
@@ -5,17 +5,35 @@ def index(request):
     allManufacturers = Products.objects.filter(category__name="Games").values_list('manufacturer', flat=True).distinct(
         'manufacturer')
 
+
     context = {'products': Products.objects.filter(category__name="Games").order_by('name'),
                'allManufacturers': allManufacturers, 'categoryName': 'games'}
 
     return render(request, 'category/index.html', context)
 
 
+
+
+
+
 def manfacturer(request, manufacturer):
     allManufacturers = Products.objects.filter(category__name="Games").values_list('manufacturer', flat=True).distinct(
         'manufacturer')
 
-    context = {'products': Products.objects.filter(category__name="Games").filter(manufacturer=manufacturer).order_by('name'),
-               'allManufacturers': allManufacturers, 'categoryName': 'games'}
+    splited_manufacturer = manufacturer.split("/")
 
-    return render(request, 'category/index.html', context)
+    if len(splited_manufacturer) > 1:
+        if splited_manufacturer[1] == 'price':
+
+            context = {
+                'products': Products.objects.filter(category__name="Games").filter(manufacturer=splited_manufacturer[0]).order_by(
+                    'price'),
+                'allManufacturers': allManufacturers, 'categoryName': 'games'}
+            return render(request, 'category/index.html', context)
+
+    else:
+        context = {'products': Products.objects.filter(category__name="Games").filter(manufacturer=manufacturer).order_by('price'),
+               'allManufacturers': allManufacturers, 'categoryName': 'games'}
+        print(context)
+
+        return render(request, 'category/index.html', context)
