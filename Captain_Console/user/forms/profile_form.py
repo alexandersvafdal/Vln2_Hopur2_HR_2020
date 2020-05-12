236d7936b2ad2e9de699ec37cab2985f599b77b3
alexandersvarfdal <alexander18@ru.is>
2020-05-12 18:41:23 +0000

    styled profiles site a bit , added a message stating that needed a name or image to be able to use cart, also styled product site

--- Captain_Console/user/forms/profile_form.py
+++ Captain_Console/user/forms/profile_form.py
@@ -4,7 +4,7 @@ from user.models import Profile
 class ProfileForm(ModelForm):
     class Meta:
         model = Profile
-        exclude = ['id', 'user']
+        exclude = ['id', 'user', 'cart']
         widgets = {
             'name': widgets.TextInput(attrs={ 'class': 'form-control'}),
             'profile_image': widgets.TextInput(attrs={'class': 'form-control'})
--- Captain_Console/user/models.py
+++ Captain_Console/user/models.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, default="User")
-    profile_image = models.CharField(max_length=9999)
-    cart = models.CharField(max_length=9999999, blank=True)
+    profile_image = models.CharField(max_length=9999, blank=True)
+    cart = models.CharField(max_length=9999999, default="{}")
