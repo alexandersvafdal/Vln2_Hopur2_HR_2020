fd311b2ce5a36d157a98106a3c923ac69405b697
alexandersvarfdal <alexander18@ru.is>
2020-05-13 13:38:12 +0000

    added firstname and lastname instead og name

--- Captain_Console/user/models.py
+++ Captain_Console/user/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
+    FirstName = models.CharField(max_length=255, blank=True)
+    LastName = models.CharField(max_length=255, blank=True)
     profile_image = models.CharField(max_length=9999, blank=True)
     cart = models.CharField(max_length=9999999, default="{}")
